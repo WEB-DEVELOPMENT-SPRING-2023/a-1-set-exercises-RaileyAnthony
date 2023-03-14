4980953820bcea4b1fbd9317548d3ccf9e52edca
Railey Inductivo <80341106+RaileyAnthony@users.noreply.github.com>
2023-03-14 22:58:15 +0400

    Create BonusExercise5.js

---
+++ Chapter-5-Embedded Content/BonusExercise5.js
@@ -0,0 +1,23 @@
+var sound1 = new Audio();
+sound1.src = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/ah-ha.mp3";
+
+var sound2 = new Audio();
+sound2.src = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/back-of-the-net.mp3";
+
+var sound3 = new Audio();
+sound3.src = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/bangoutoforder.mp3";
+
+var sound4 = new Audio();
+sound4.src = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/dan.mp3";
+
+var sound5 = new Audio();
+sound5.src = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/emailoftheevening.mp3";
+
+var sound6 = new Audio();
+sound6.src = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/hellopartridge.mp3";
+
+var sound7 = new Audio();
+sound7.src = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/iateascotchegg.mp3";
+
+var sound8 = new Audio();
+sound8.src = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/imconfused.mp3";
