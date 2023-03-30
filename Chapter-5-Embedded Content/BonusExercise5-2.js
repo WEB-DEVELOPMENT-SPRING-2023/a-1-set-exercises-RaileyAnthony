08f99d9f0494c44cabdd94ae1667d9f668184ba2
Railey Inductivo <80341106+RaileyAnthony@users.noreply.github.com>
2023-03-30 17:36:57 +0400

    commit

--- Chapter-5-Embedded Content/BonusExercise5-2.js
+++ Chapter-5-Embedded Content/BonusExercise5-2.js
@@ -25,3 +25,30 @@ sound8.src = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/imconfus
 var sound9 = new Audio();
 sound9.src = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/conman.mp3";
 
+var sound10 = new Audio();
+sound10.src = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/a-bit-of-a-maverick.mp3";
+
+var sound11 = new Audio();
+sound11.src = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/a-trapped-boy.mp3";
+
+var sound12 = new Audio();
+sound12.src = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/access-to-dixons.mp3";
+
+var sound13 = new Audio();
+sound13.src = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/annie-lennox.mp3";
+
+var sound14 = new Audio();
+sound14.src = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/aqua.mp3";
+
+var sound15 = new Audio();
+sound15.src = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/arm-wrestling.mp3";
+
+var sound16 = new Audio();
+sound16.src = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/billwithers.mp3";
+
+var sound17 = new Audio();
+sound17.src = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/bangkok.mp3";
+
+var sound18 = new Audio();
+sound18.src = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/gadzooks.mp3";
+
